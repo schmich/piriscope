2885c3905ae5576c8a6aabab1b035f2470d9bf22
Chris Schmich <schmch@gmail.com>
2017-04-21 05:59:58 -0500

    Fix error in Dockerfile. Short-circuit Docker build script if command fails.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM debian:latest
 MAINTAINER Chris Schmich <schmch@gmail.com>
-RUN apt-get update
+RUN apt-get update \
  && apt-get install -y git dh-make dpkg-dev debhelper devscripts
 COPY . /src
 WORKDIR /src
--- build-docker.rb
+++ build-docker.rb
@@ -3,8 +3,8 @@ package = "#{project}_1.0-1_all.deb"
 
 Dir.chdir(File.dirname(__FILE__))
 
-system("docker build -t #{project} .")
-system("docker run -it #{project}")
+system("docker build -t #{project} .") || fail
+system("docker run -it #{project}") || fail
 id = `docker ps -l -q`.strip
-system("docker cp '#{id}:/src/#{package}' .")
-system("docker rm #{id}")
+system("docker cp '#{id}:/src/#{package}' .") || fail
+system("docker rm #{id}") || fail
